c7dc59318a93d31a50a4ce7b326f557685860c00
79181 <katerina.tokan@yandex.ru>
2022-11-29 15:57:49 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
+	public static void reverse(int [] nums){
+		int [] temp = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			temp[nums.length - 1 - i] = nums[i];
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = temp[i];
+
+		}
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world!");
+		int [] nums = {1,2,3,4,5,6,7,8,9};
+		reverse(nums);
+
+		System.out.println(Arrays.toString(nums));
+
 	}
 }
